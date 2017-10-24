9b531a704ceb709ecf13edb6fd8b3ca1209b8a2e
avincent6 <vincent6@purdue.edu>
2017-10-24 18:03:29 -0400

    hide footer if not authenticated

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../../css/footer.css';
 import edit_icon_white from '../../assets/pencil-icon.svg';
+import { withCookies } from 'react-cookie';
 
-const Footer = () => (
-  <div className="footer">
-    <button className="button"><img src={edit_icon_white} alt="edit_icon_white"/></button>
-    <button className="button">?</button>
-  </div>
-);
+const Footer = ({ cookies }) => {
+  const isAuthenticated = cookies.get('isAuthenticated');
+  if (isAuthenticated === "true") {
+    return (
+      <div className="footer">
+        <button className="button"><img src={edit_icon_white} alt="edit_icon_white"/></button>
+        <button className="button">?</button>
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
 
-export default Footer;
+export default withCookies(Footer);
