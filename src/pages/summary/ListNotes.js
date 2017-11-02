faad42a4256bc62e6e6097aea6017ea4e1051447
avincent6 <vincent6@purdue.edu>
2017-11-02 18:39:41 -0400

    update list component with api call to /listnotes

--- src/pages/summary/ListNotes.js
+++ src/pages/summary/ListNotes.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
-import { withCookies, Cookies } from 'react-cookie';
+import { withCookies } from 'react-cookie';
 import { Redirect } from 'react-router-dom';
-// import apiFetch from '../../utils/api.js';
+import apiFetch from '../../utils/api.js';
 import '../../css/summary.css';
 
 class Summary extends Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //
-    // };
+    this.state = {
+      notes: []
+    };
+  }
+  componentDidMount() {
+    const { cookies } = this.props;
+    const email = cookies.get('email');
+    apiFetch('listnotes', {
+      headers: {
+       'Accept': 'application/json',
+       'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email
+      }),
+      method: 'POST'
+    }).then(response =>
+      response.json()
+    ).then((json) => {
+        if (json.success === false) {
+            console.log('error', json.error);
+        }
+        else {
+          console.log('success',json);
+          this.setState({
+            notes: json
+          });
+        }
+      });
   }
   render() {
     const { cookies } = this.props;
@@ -17,9 +43,16 @@ class Summary extends Component {
     if (isAuthenticated === "false" || !isAuthenticated) {
       return (<Redirect to="/login"/>);
     }
+
     return (
       <div className="summary">
         <h1>My Notes</h1>
+        {this.state.notes.length > 0 ?
+          this.state.notes.map(note => {
+            return (<div><h2>{note.name}</h2><p>{note.noteId}</p></div>);
+          })
+          : null
+        }
       </div>
     );
   }
