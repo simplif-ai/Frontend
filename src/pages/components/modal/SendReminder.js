e98183a09670423741c2cb01f9f383b28faeee89
avincent6 <vincent6@purdue.edu>
2017-12-01 18:10:19 -0500

    add correct placeholder for date

--- src/pages/components/modal/SendReminder.js
+++ src/pages/components/modal/SendReminder.js
@@ -17,17 +17,21 @@ class ReminderModal extends React.Component {
     handleInput = (e) =>  {
       this.setState({ text: e.target.value });
     }
-    addToCalendar = (date) => {
+    selectDate = (date) => {
       console.log('add to calendar: ', date);
-      
+      this.setState({
+        dateSelect: true,
+        selectedDate: date
+      });
     }
     render() {
         console.log('dateFound', this.props.dateFound, 'dates', this.state.dates);
+        console.log('this.state.dateSelect', this.state.dateSelect);
         const dates = [];
         if (this.state.dates && this.state.dates.length > 0) {
           this.state.dates.forEach(date => {
             console.log('date in for each', date);
-            dates.push(<a className="block" onClick={() => this.addToCalendar(date)}>{date}</a>);
+            dates.push(<a className="block" onClick={() => this.selectDate(date)}>{date}</a>);
           });
         }
         return (
@@ -59,9 +63,11 @@ class ReminderModal extends React.Component {
                 <form onSubmit={this.addDate}>
                   <h2>Edit Your Event</h2>
                   <label htmlFor="summary">Event Title</label>
-                  <input type="text" name="summary" required />
-                  <label htmlFor="date">Start Date</label>
-                  <input type="datetime-local" name="date" required />
+                  <input type="text" name="summary" placeholder={`Event on ${this.state.selectedDate}`} required />
+                  <label htmlFor="start-date">Start Date</label>
+                  <input type="datetime-local" name="start-date" required />
+                  <label htmlFor="end-date">End Date</label>
+                  <input type="datetime-local" name="end-date" required />
                   <input className="btn" type="submit" name="submit" value="Save Event in Google Calendar" />
                 </form>
                 :
