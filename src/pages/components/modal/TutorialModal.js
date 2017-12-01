10068b508198cc64dc4a98ff8703c02686169cc2
Ria <chocolodoro@gmail.com>
2017-12-01 16:57:00 -0500

    starting to fix tutorial; deleted extra 'create simplifai folder' in listnotes

--- src/pages/components/modal/TutorialModal.js
+++ src/pages/components/modal/TutorialModal.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ModalWrapper from './ModalWrapper';
 
 import '../../../css/tutorial.css';
-import img1 from '../../../assets/profile_ss.png';
-import img2 from '../../../assets/folders_ss.png';
-import img3 from '../../../assets/notes_ss.png';
+import img1 from '../../../assets/simplifai_profile.png';
+import img2 from '../../../assets/simplifai_notes1.png';
+import img3 from '../../../assets/simplifai_notes2.png';
+import img4 from '../../../assets/simplifai_summarize.png';
+import img5 from '../../../assets/simplifai_feedback.png';
 
 class TutorialModal extends React.Component {
     constructor(props) {
@@ -13,7 +15,9 @@ class TutorialModal extends React.Component {
         slideIndex: 1,
         slides: [img1,img2,img3],
         tutText:[
-          "explanatory text1","text2","text3!"
+          "The profile page lets you handle user settings! Edit your name and email in 'Edit Profile', toggle your email preferences, delete your account (careful with that one!),authorize your account with Google, and view previously submitted feedback!",
+          "In 'My Notes', you handle your notes. You can create a new folder to store notes in, summarize an article by URL, pdf, or by pptx, or just click the '+' button at the top to create a new summary!",
+          "If you scroll down 'My Notes', you can schedule email reminders for yourself to study, or "
         ]
     };
   }
@@ -35,7 +39,7 @@ class TutorialModal extends React.Component {
             toggleState={this.props.toggleState}
           >
           <div className="carousel">
-            <img src= {this.state.slides[(this.state.slideIndex%3)]} style={{'width':'60%'}}alt = "slide"/>
+            <img className="carousel-img" src= {this.state.slides[(this.state.slideIndex%3)]} style={{'width':'60%'}}alt = "slide"/>
             <p>{this.state.tutText[this.state.slideIndex%3]}</p>
           </div>
            <i className="arrow leftArrow" onClick={this.clickPrev}></i>
